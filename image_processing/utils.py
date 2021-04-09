a3e94af425510a817fa378730261a070a4db5054
unknown <TOSHIHIRORYUU96@GMAIL.COM>
2021-04-09 11:49:44 +0530

    rgb_to_grey

--- image_processing/utils.py
+++ image_processing/utils.py
@@ -1,8 +1,10 @@
+import cv2
 
-
-
+# convert rgb images to grey-scale images.
 def rgb_to_grey(rgb_img):
+    grey_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
     return grey_img
 
+# convert grey-scale images to rgb images.
 def grey_to_rgb(grey_img):
     return rgb_img
