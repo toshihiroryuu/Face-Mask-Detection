2c2fa494e97c50e6cc71d520776fa93e8189eef3
Athul Mathew <48464848+toshihiroryuu@users.noreply.github.com>
2021-04-09 11:56:17 +0530

    Update utils.py

--- image_processing/utils.py
+++ image_processing/utils.py
@@ -1,10 +1,10 @@
 import cv2
 
-# convert rgb images to grey-scale images.
+# convert rgb image to grey-scale image.
 def rgb_to_grey(rgb_img):
     grey_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
     return grey_img
 
-# convert grey-scale images to rgb images.
+# convert grey-scale image to rgb image.
 def grey_to_rgb(grey_img):
     return rgb_img
